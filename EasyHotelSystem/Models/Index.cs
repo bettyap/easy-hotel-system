c8bc6224f16554556cb4b16db40a7a945d76c846
bettyap <beth_ap@live.com>
2019-11-06 02:13:18 -0300

    acrescentado index

---
+++ EasyHotelSystem/Models/Index.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyHotelSystem.Models
+{
+    public class Index
+    {
+        public int num;
+    }
+}
